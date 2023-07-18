68283de301cd11381ccf5b49e77a9af89527a94a
Qkrwnsgus0522 <harveydent0522@gmail.com>
2023-07-18 16:07:15 +0900

    찜 페이지, 추가, 삭제 Action 수정

--- src/main/java/controller/InsertFavorAction.java
+++ src/main/java/controller/InsertFavorAction.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.FavorDAO;
+import model.FavorVO;
 
 public class InsertFavorAction implements Action {
 
@@ -12,10 +16,21 @@ public class InsertFavorAction implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		ActionForward forward = new ActionForward();
+		ActionForward forward = null;
 		
-//		forward.setRedirect(true);
-//		forward.setPath("");
+		FavorDAO fDAO = new FavorDAO();
+		FavorVO fVO = new FavorVO();
+		
+		HttpSession session = request.getSession();
+		
+		fVO.setMemberId((String)session.getAttribute("ssMemberId"));
+		fVO.setTeaNum(Integer.parseInt(request.getParameter("teaNum")));
+		
+		if (fDAO.insert(fVO)) {
+			forward = new ActionForward();
+			forward.setRedirect(true);
+			forward.setPath("favor.jsp"); // 경로 수정 필요
+		}
 		
 		return forward;
 	}
